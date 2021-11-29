0bd91d2fe3c5e377d1e46f9672b1daf864e6c86c
Bogdan A <arsenebp@gmail.com>
2021-11-29 23:18:21 +0200

    Update run_tests.sh
    needs to install truffle globally :|

--- run_tests.sh
+++ run_tests.sh
@@ -8,12 +8,12 @@
 read -n1 -p "Test locally or fork Mumbai? press [l]Locally, [m]Mumbai" option 
 case $option
 in
-     m) npm install ;
+     m) npm install && npm install -g truffle;
         ganache-cli -f https://polygon-mumbai.g.alchemy.com/v2/uMH2G0KHfYWLBcH0SOdEbF97o64eGzSs -u 0xdCB67264d028bd19aC242962F2f56bDb041c02b9  & 
         truffle test --network forkedmumbai && kill -KILL $(jobs -p) ;
         echo "----YOU HAVE REACHED THE END-----" ;;
      
-     l) npm install && truffle test && kill -9 $(jobs -p) ;;
+     l) npm install && npm install -g truffle && truffle test && kill -9 $(jobs -p) ;;
         
      *) echo -n "no rush. take your time. choose wisely." ;;
 esac
